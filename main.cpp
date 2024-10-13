9f4557a8ee74eec52c755c1e66b470aac9bf94e5
termbit <asphodel-corp@proton.me>
2024-10-13 18:44:16 +0400

    Added a choosen filename output

--- main.cpp
+++ main.cpp
@@ -7,13 +7,20 @@
 #include <FL/Fl_File_Chooser.H> 
 #include <FL/fl_ask.H>
 #include <string>
+#include <FL/Fl_Output.H>
 #include <fstream>
+#include <cstring>
 
 const char* user_filename;
 const char* user_givven_text;
 
 void file_chooser(Fl_Widget* widget, void* data) {
 	user_filename = fl_file_chooser("", "", "");
+    
+    Fl_Output* choosen_filename = static_cast<Fl_Output*>(data);
+    const char* filename_for_output = strrchr(user_filename, '/');
+
+    choosen_filename->value(filename_for_output + 1);
 }
 
 void find_in_file(Fl_Widget* widget, void* data) {
@@ -66,15 +73,21 @@ void find_in_file(Fl_Widget* widget, void* data) {
 int main(int argc, char **argv) {
 
 	Fl_Window *window = new Fl_Window(500,300, "Search in a file");
-	Fl_Button *button = new Fl_Button(190, 90, 120, 30, "Choose a file");
-	button->callback(file_chooser);
-	
+	Fl_Button *file_choose_button = new Fl_Button(185, 90, 130, 30, "Choose a file");
+	file_choose_button->labelsize(16);
+
+    Fl_Output *choosen_filename = new Fl_Output(350, 90, 100, 30);
+
+	file_choose_button->callback(file_chooser, choosen_filename);
+
 	Fl_Input *text_to_find = new Fl_Input(150, 140, 200, 30, "Text for find: ");
+    text_to_find->labelsize(16);
+    
 
-	Fl_Button *find_button = new Fl_Button(210, 220, 80, 30, "SEARCH");
+	Fl_Button *find_button = new Fl_Button(205, 220, 90, 30, "SEARCH");
+    find_button->labelsize(17);
 	find_button->callback(find_in_file, text_to_find);
-
-
+    
 
 	window->end();
 	window->show(argc, argv);
